d88e94a144b58684184cd5325412f693d4bdb93c
Isabella Bejarano Lopez <ibejaranol@eafit.edu.co>
2026-02-08 22:29:00 -0500

    segundo commit

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    AGE_CHOICES = [
+        ('kids', 'Niños'),
+        ('teens', 'Jóvenes'),
+        ('adults', 'Adultos'),  
+    ]
+    age_group = models.CharField(max_length=10, choices = AGE_CHOICES, default='teens')
+    image = models.ImageField(upload_to= 'movie/images/')
+    url = models.URLField(blank=True)
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
-# Create your views here.
-def home(request) :
-    # return HttpResponse('<h1>Welcome to the page</h1>')
-    return render(request, 'home.html', {'name': 'Isabella Bejarano'})
+def home(request):
+    age = request.GET.get("age", "")               # kids / teens / adults
+    searchTerm = request.GET.get("searchMovie", "")
+
+    movies = Movie.objects.all()
+
+    if age:
+        movies = movies.filter(age_group=age)
+
+    if searchTerm:
+        movies = movies.filter(title__icontains=searchTerm)
+
+    return render(request, "home.html", {
+        "searchTerm": searchTerm,
+        "movies": movies
+    })
+
+def about(request):
+    return HttpResponse("<h1>Welcome to about page</h1>")
